8e39460e94abfacd011521704fd88acc59841b08
KavyaSree <chirramanasa06@gmail.com>
2025-10-07 15:44:38 +0530

    adding two files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:21-jdk-slim
+
+WORKDIR /app
+
+COPY samplename.java .
+
+RUN javac samplename.java
+
+CMD ["java", "-cp", ".", "samplename"]
---
+++ samplename.java
@@ -0,0 +1,8 @@
+public class samplename
+{
+    public static void main(String args[])
+    {
+        String name="KavyaSree";
+        System.out.println("Hello,My name is",+name);
+    }
+}
